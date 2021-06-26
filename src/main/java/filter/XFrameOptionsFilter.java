ce0bc44339a2b9bf47815c7311491e15d27f8da2
J.Tan <j.tan-1@student.utwente.nl>
2021-06-26 15:55:13 +0200

    Update security filters and change paths to be './Topicus/...'

---
+++ src/main/java/filter/XFrameOptionsFilter.java
@@ -0,0 +1,43 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebFilter(filterName = "XFrameOptionsFilter", urlPatterns = {"/*"})
+public class XFrameOptionsFilter implements Filter {
+	
+	private static final Logger log = LoggerFactory.getLogger(XFrameOptionsFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        log.info("X-Frame-Options header added to response");
+
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.addHeader("X-Frame-Options", "DENY");
+        response.addHeader("X-Frame-Options", "SAMEORIGIN");
+        response.addHeader("X-Frame-Options", "ALLOW-FROM http://localhost:8080/Topicus/");
+        //response.addHeader("X-Frame-Options", "ALLOW-FROM http://topicus-bank1.paas.hosted-by-previder.com/Topicus/");
+        
+        filterChain.doFilter(servletRequest, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
