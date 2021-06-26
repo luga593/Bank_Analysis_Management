fd0dd3be974d0491c94aa1455ff4e17dcd655758
Victor Mintus <s.mintus@student.utwente.nl>
2021-06-26 11:01:29 +0200

    changed path of driver

--- src/main/java/seleniumtesting/FirstTest.java
+++ src/main/java/seleniumtesting/FirstTest.java
@@ -1,5 +1,6 @@
 package seleniumtesting;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
@@ -17,8 +18,9 @@ public class FirstTest {
 		// setting the driver executable
 		// System.setProperty("webdriver.chrome.driver", "chromedriver");
 		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\victo\\Desktop\\Technical Computer Science\\Year1\\Module 4\\Topicus\\chromedriver.exe");
+				"chromedriver.exe");
 		// Initiating your chromedriver
+		//System.out.println(new File(System.getProperty("C:\\Users\\victo\\Desktop\\Technical Computer Science\\Year1\\Module 4\\Topicus\\src\\main\\java\\seleniumtesting\\FirstTest.java")).getParent());
 		WebDriver driver = new ChromeDriver();
 		// Applied wait time
 		// driver.manage().timeouts().implicitlyWait(10000, TimeUnit.SECONDS);
@@ -36,8 +38,25 @@ public class FirstTest {
 		driver.findElement(By.id("login-password")).sendKeys("user2");
 		driver.findElement(By.cssSelector("button[class='btn-login']")).click();
 		if (driver.getCurrentUrl().equals("http://localhost:8080/Topicus/mainPage.jsp")) {
+			//System.out.println("yes");
+		}
+		driver.findElement(By.linkText("Processes & Transactions")).click();
+		driver.findElement(By.xpath("//button[text()='Get table']")).click();
+		System.out.println(driver.findElement(By.id("table")).isDisplayed());
+		if(driver.findElement(By.id("table")).isDisplayed()) {
 			System.out.println("yes");
 		}
+		if (driver.getCurrentUrl().equals("http://localhost:8080/Topicus/Table.jsp")) {
+			//System.out.println("yes");
+		}
+		driver.get("http://localhost:8080/Topicus/mainPage.jsp");
+		driver.findElement(By.linkText("Bank Statement Upload")).click();
+		if (driver.getCurrentUrl().equals("http://localhost:8080/Topicus/upload.jsp")) {
+			//System.out.println("yes");
+		}
+		driver.findElement(By.className("loginButton"));
+		driver.get("http://localhost:8080/Topicus/mainPage.jsp");
+		
 		;
 		// Assert.assertTrue(((RemoteWebDriver)
 		// driver).findElementByCssSelector("button[class='w3-button w3-black
