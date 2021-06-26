01df4a5cabc205bd361d02e2117ebac493299a2a
Victor Mintus <s.mintus@student.utwente.nl>
2021-06-26 19:30:56 +0200

    test cases for login and uploadpage

---
+++ src/main/java/seleniumtesting/Testing.java
@@ -0,0 +1,64 @@
+package seleniumtesting;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class Testing {
+	private static WebDriver driver;
+	@BeforeAll
+	public static void setDriver() {
+		System.setProperty("webdriver.chrome.driver",
+				"chromedriver.exe");
+		Testing.driver = new ChromeDriver();
+		Testing.driver.manage().window().maximize();
+		Testing.driver.manage().deleteAllCookies();
+		Testing.driver.get("http://localhost:8080/Topicus/newMainPage.jsp");
+		System.out.println(driver);
+	}
+	
+	@Test
+	public void testlogin() {
+		//setDriver();
+		//System.out.println(WebTesting.driver);
+		driver.findElement(By.cssSelector("button[class='w3-button w3-black w3-padding-large w3-large w3-margin-top']"))
+		.click();
+		Assertions.assertTrue(driver.getCurrentUrl().equals("http://localhost:8080/Topicus/testLogin.jsp"));
+		driver.findElement(By.id("login-username")).sendKeys("user1");
+		driver.findElement(By.id("login-password")).sendKeys("user2");
+		driver.findElement(By.cssSelector("button[class='btn-login']")).click();
+		Assertions.assertTrue(driver.getCurrentUrl().equals("http://localhost:8080/Topicus/mainPage.jsp"));
+		driver.findElement(By.id("log-out")).click();
+		Assertions.assertTrue(driver.getCurrentUrl().equals("http://localhost:8080/Topicus/testLogin.jsp"));
+		driver.findElement(By.id("login-username")).sendKeys("user1");
+		driver.findElement(By.id("login-password")).sendKeys("User2");
+		driver.findElement(By.cssSelector("button[class='btn-login']")).click();
+		Assertions.assertTrue(driver.getCurrentUrl().equals("http://localhost:8080/Topicus/failedLogin.jsp"));	
+	}
+	
+	public void login() {
+		//setDriver();
+		Testing.driver.get("http://localhost:8080/Topicus/newMainPage.jsp");
+		driver.findElement(By.cssSelector("button[class='w3-button w3-black w3-padding-large w3-large w3-margin-top']"))
+		.click();
+		driver.findElement(By.id("login-username")).sendKeys("user1");
+		driver.findElement(By.id("login-password")).sendKeys("user2");
+		driver.findElement(By.cssSelector("button[class='btn-login']")).click();
+		
+	}
+	@Test
+	public void testUpload() {
+		login();
+		driver.findElement(By.linkText("Bank Statement Upload")).click();
+		Assertions.assertTrue(driver.getCurrentUrl().equals("http://localhost:8080/Topicus/upload.jsp"));
+		
+	}
+	
+
+
+}
